1d97ffce9d104887cd4a2bfc434a59dec5f51b0e
Akash Kumar Gupta <49365126+akashx2311@users.noreply.github.com>
2023-02-20 16:06:22 +0000

    Creating tree for the  practice

--- .ccls-cache/@home@runner@Tree-Exercise/Build_tree.cpp
+++ .ccls-cache/@home@runner@Tree-Exercise/Build_tree.cpp
@@ -11,15 +11,43 @@ public:
     right = nullptr;
   }
 };
-Node* 
+
+int height(Node *root) {
+  if (root == nullptr)
+    return 0;
+  return 1 + max(height(root->left), height(root->right));
+}
+
+vector<int> level_order(Node *root) {
+  vector<int> ans;
+  if (root == NULL)
+    return ans;
+  queue<Node *> q;
+  q.push(root);
+  while (!q.empty()) {
+    Node *temp = q.front();
+    q.pop();
+    ans.push_back(temp->val);
+    if (temp->left)
+      q.push(temp->left);
+    if (temp->right)
+      q.push(temp->right);
+  }
+  return ans;
+}
+
 int main() {
 
+  Node *root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
 
-  Node* root ;
-
-  
-
-
-
+  cout<<height(root)<<endl;
+  vector<int> ans = level_order(root);
+  for(auto i : ans)
+    {
+      cout<<i<<" ";
+      
+    }
   
 }
