f9202c321d2b6c3cdbf2e0e30761829ad557dd24
Akash Gupta <>
2023-02-20 16:03:41 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Tree-Exercise/Build_tree.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *left, *right;
+  Node(int x) {
+    val = x;
+    left = nullptr;
+    right = nullptr;
+  }
+};
+Node* 
+int main() {
+
+
+  Node* root ;
+
+  
+
+
+
+  
+}
